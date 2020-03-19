95ffee7f7e8c4cb2cac57cf7124b0cf5bcb1186a
Lillian Burnside <lillian.burnside@gmail.com>
2020-03-19 15:35:40 -0500

    heroku inital commit

--- plant-app-server/src/config.js
+++ plant-app-server/src/config.js
@@ -3,6 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://plantuser@localhost/plant_app',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://plantuser@localhost/plant_app',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://plantuser@localhost/plant_app_test'
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
